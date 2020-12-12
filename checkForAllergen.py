65953143ac63ecc523900523644f265d27909cf5
Tang-Jensen <markustang@hotmail.com>
2020-12-12 15:57:12 +0100

    checkforallergens added

---
+++ checkForAllergen.py
@@ -0,0 +1,81 @@
+import re
+
+milkIngredients = [
+    "mælk",
+    "smør",
+    "ost",
+    "yoghurt",
+    "ymer",
+    "ylette",
+    "fløde",
+    "milkshake",
+    "bagermargarine",
+    "margarine",
+    "minarine",
+]
+
+glutenIngredients = [
+    "hvede",
+    "rug",
+    "byg",
+    "bulgur",
+    "couscous",
+    "emmer",
+    "mannagryn",
+    "semolina",
+    "semulje",
+    "spelt"
+]
+
+nutIngredients = [
+    "mandel",
+    "hasselnød",
+    "valnød",
+    "cashewnød",
+    "pekannød",
+    "paranød",
+    "pistacienød",
+    "queenslandnød",
+    "macadamianød"
+]
+
+def checkForMilkAllergy(ingredients):
+    tmp = ingredients.lower().split(",")
+    tmp[0] = tmp[0].split()[-1]
+    out = []
+    for i in range(len(tmp)):
+        for j in range(len(milkIngredients)):
+            if re.search(milkIngredients[j], tmp[i]):
+                out.append(tmp[i])
+    return (out, (len(out) > 0))
+
+def checkForGlutenAllergy(ingredients):
+    tmp = ingredients.lower().split(",")
+    tmp[0] = tmp[0].split()[-1]
+    out = []
+    for i in range(len(tmp)):
+        for j in range(len(glutenIngredients)):
+            if re.search(glutenIngredients[j], tmp[i]):
+                out.append(tmp[i])
+    return (out, (len(out) > 0))
+
+def checkForNutAllergy(ingredients):
+    tmp = ingredients.lower().split(",")
+    tmp[0] = tmp[0].split()[-1]
+    out = []
+    for i in range(len(tmp)):
+        for j in range(len(nutIngredients)):
+            if re.search(nutIngredients[j], tmp[i]):
+                out.append(tmp[i])
+    return (out, (len(out) > 0))
+
+def checkForAllergy(ingredients, allergens):
+    tmp = ingredients.lower().split(",")
+    tmp[0] = tmp[0].split('-')[1]
+    out = []
+    print(allergens)
+    for i in range(len(tmp)):
+        for j in range(len(allergens)):
+            if re.search(allergens[j], tmp[i]):
+                out.append(tmp[i])
+    return (out, (len(out) > 0))
--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 import sallingUtils as salling
-import tkinter as tk
+import checkForAllergen as check
+import sys
 
-
-class Main:
-    def __init__(self):
-        self.window = tk.Tk(className=".Allergen App")
-        self.window.geometry("600x400")
-
-        tk.Label(self.window, text="Find Produkt").grid(row=0)
-        self.e1 = tk.Entry(self.window)
-        self.e1.grid(row=0, column=1)
-
-        self.displayText = tk.Text(self.window, height=40, width=40, foreground="black", font=("Helvetica", 16), wrap=tk.WORD)
-        self.displayText.grid(row=3, column=2)
-        self.displayText.insert(1.0, "test")
-        self.displayText.configure(state='disabled')
-        tk.Button(self.window, highlightbackground="black", text="Search", command=self.changeText).grid(row=1, column=0)
-
-        self.window.mainloop()
-
-    def changeText(self):
-        if len(self.e1.get()) > 0:
-            self.displayText.configure(state='normal')
-            self.displayText.delete(1.0, "end")
-            self.displayText.insert(1.0, str(salling.find_ingredients(self.e1.get())))
-            self.displayText.configure(state='disabled')
-
-
-app = Main()
+ingredients = salling.find_ingredients(sys.argv[1])
+allergens = sys.argv[2].split(",")
+print(check.checkForAllergy(ingredients, allergens))
